f1ac2f6ee167b488820573717130dc52a2ddddb1
Christos Mourtogiannis <c.mourtogiannis@gmail.com>
2025-01-14 17:07:36 +0200

    feat: Disable overview on startup via `dash-to-dock` extension

--- gnome/scripts/extensions.sh
+++ gnome/scripts/extensions.sh
@@ -14,6 +14,7 @@ gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
 gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.5
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock disable-overview-on-startup true
 
 # Blur configuration.
 gsettings set org.gnome.shell.extensions.blur-my-shell brightness 0.5
