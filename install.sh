0121a0cc8dbf8b8b090dd06d4b8fef768b55d827
Christos Mourtogiannis <c.mourtogiannis@gmail.com>
2025-04-28 01:06:01 +0300

    feat: Enhance installation script for improved OS detection and configuration

--- install.sh
+++ install.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
-# Copy the needed desktop configuration files to the home directory.
-cp -R .config/* ~/.config/
+# Catch exit signal (`CTRL` + `C`) to terminate the whole script.
+trap "exit" INT
 
-# TODO: Make the installation OS agnostic.
-# Install the needed dependencies.
-paru -S --noconfirm --needed - < packages/dependencies.txt
+# Terminate script on error.
+set -e
 
-# Install the applications.
-paru -S --noconfirm --needed - < packages/applications.txt
+# Constant variable of the scripts' working directory to use for relative paths.
+INSTALL_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# Configure GNOME.
-sh gnome/install.sh
+# Import functions and flags.
+source "$INSTALL_SCRIPT_DIRECTORY/helpers/logs.sh"
+
+# Copy the needed configuration files to the new machine.
+log_info "Copying configuration files to '~/.config'..."
+mkdir -p ~/.config && cp -R .config/* ~/.config/
+
+log_info "Copying 'Git' configuration..."
+cp "$INSTALL_SCRIPT_DIRECTORY/.gitconfig" ~
+
+# Detect `OS` and run the appropriate configuration script.
+os_name=$(uname -s)
+if [ "$os_name" = "Darwin" ]; then
+  # Configure `macOS`.
+  log_info "Starting 'macOS' configuration..."
+  sh macos/configure.sh
+elif [ "$os_name" = "Linux" ]; then
+  # Check for `Arch Linux` specifically.
+  if [ -f /etc/os-release ] && grep -q '^ID=arch$' /etc/os-release; then
+    # Configure `Linux`.
+    log_info "Starting 'Linux' configuration..."
+    sh linux/configure.sh
+  else
+    log_error "Unsupported 'Linux' system!"
+    exit 1
+  fi
+else
+  log_error "Unsupported operating system!"
+  exit 1
+fi
+
+log_success "System configuration completed!"
+
+# Reboot system.
+log_info "Initiating system reboot..."
+sudo reboot
