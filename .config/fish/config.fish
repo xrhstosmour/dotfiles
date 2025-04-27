7825da681ed622ad8eab898c247fa665ad3c711f
Christos Mourtogiannis <c.mourtogiannis@gmail.com>
2025-04-28 01:08:53 +0300

    feat: Update `Fish` configuration to enhance `macOS` configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,19 @@
-# Enable starship at fish prompt.
+# Add `Homebrew`'s binary directory to the `PATH` environment variable.
+set kernel_name (uname -s)
+if test "$kernel_name" = "Darwin"
+  fish_add_path "/opt/homebrew/bin/"
+end
+
+# Disable `Homebrew` environment update hints.
+set -gx HOMEBREW_NO_ENV_HINTS 1
+
+# Enable `Starship` as `Fish` prompt.
 starship init fish | source
 
-# Enable zoxide at fish prompt.
+# Enable `Zoxide` at `Fish` shell.
 zoxide init fish | source
 
-# Enable and configure atuin at fish prompt.
+# Enable and configure `Atuin` at `Fish` shell.
 set -gx ATUIN_NOBIND "true"
 atuin init fish | source
 bind \ch _atuin_search
@@ -12,3 +21,17 @@ bind -M insert \ch _atuin_search
 
 # Disable welcome message.
 set -U fish_greeting
+
+# Source needed `Fish` constants.
+source $HOME/.config/fish/constants/colors.fish
+
+# Source needed `Fish` functions.
+source $HOME/.config/fish/functions/files.fish
+source $HOME/.config/fish/functions/git.fish
+source $HOME/.config/fish/functions/logs.fish
+
+# Source `Linux` specific `Fish` functions.
+if test "$kernel_name" = "Linux"
+  source $HOME/.config/fish/functions/copy.fish
+  source $HOME/.config/fish/functions/trashy.fish
+end
