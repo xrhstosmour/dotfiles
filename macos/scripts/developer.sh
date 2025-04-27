4a9b85c274eed090a5bc399bf6ce39b4c24e8dbb
Christos Mourtogiannis <c.mourtogiannis@gmail.com>
2025-04-28 01:04:41 +0300

    feat: Add developer script to install `Xcode` command line tools

---
+++ macos/scripts/developer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Catch exit signal (`CTRL` + `C`) to terminate the whole script.
+trap "exit" INT
+
+# Terminate script on error.
+set -e
+
+# Constant variable of the scripts' working directory to use for relative paths.
+DEVELOPER_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Import functions and flags.
+source "$DEVELOPER_SCRIPT_DIRECTORY/../../helpers/logs.sh"
+
+# Install developer tools.
+if ! xcode-select -p &>/dev/null; then
+  log_info "Installing 'Xcode' command line tools..."
+  xcode-select --install
+  log_info "Please follow on-screen instructions for 'Xcode' tools."
+else
+  log_warning "'Xcode' command line tools are already installed."
+fi
