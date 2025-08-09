a990974f6fc9233853621883892883420385dfe9
Christos Mourtogiannis <c.mourtogiannis@gmail.com>
2025-08-09 16:51:11 +0300

    feat: Add functionality to reload `macOS` configured environment

--- install.sh
+++ install.sh
@@ -12,16 +12,26 @@ INSTALL_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Import functions and flags.
 source "$INSTALL_SCRIPT_DIRECTORY/helpers/logs.sh"
 
+# Detect `OS` and run the appropriate configuration script.
+os_name=$(uname -s)
+
 # Copy the needed configuration files to the new machine.
 log_info "Copying configuration files to '~/.config'..."
 mkdir -p ~/.config && cp -R .config/* ~/.config/
+
+# On macOS, also copy `keyboard.sh`, without any logs, in order to reload them.
+os_name=$(uname -s)
+if [ "$os_name" = "Darwin" ]; then
+  sed '/^log_\w\+()/,/^}/d;/log_\w\+ /d;/^source /d' "$INSTALL_SCRIPT_DIRECTORY/utilities/macos/keyboard.sh" > ~/.config/scripts/utilities/macos/keyboard.sh
+fi
+
+# Give execute permission to all scripts in the directory.
+chmod +x ~/.config/scripts/utilities/macos/*.sh
 
 log_info "Copying 'Git' configuration..."
 cp "$INSTALL_SCRIPT_DIRECTORY/.gitconfig" ~
 log_divider
 
-# Detect `OS` and run the appropriate configuration script.
-os_name=$(uname -s)
 if [ "$os_name" = "Darwin" ]; then
   # Configure `macOS`.
   log_info "Starting 'macOS' configuration..."
